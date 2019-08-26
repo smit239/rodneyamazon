8c22f06427e0cf6503dea1bff67a23c39f2e352b
Rodney Smith <smit239@gmail.com>
2019-08-26 13:58:59 -0400

    heroku

--- models/Product.js
+++ models/Product.js
@@ -1,2 +1,17 @@
-const Project = sequelize.define('project', {
-    product_name
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    product_name: DataTypes.STRING
+  }, {});
+  Product.associate = function(models) {
+    // associations can be defined here
+  };
+  return Product;
+  const Department = sequelize.define('Department', {
+    Department_name: DataTypes.STRING
+  }, {});
+ Department.associate = function(models) {
+    // associations can be defined here
+  };
+  return Department;
+};
